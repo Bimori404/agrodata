ed74ee2ef7e5e058869a0416047c7fec987f098c
Jordanius <jordaniusbusiness@gmail.com>
2023-06-06 22:30:56 -0600

    v1

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+// Cards
+//import UserCard from "../pages/UserProfile/UserCard.vue";
+//export { UserCard, EditProfileForm };
+
+// Forms
+import EditProfileForm from "../pages/UserProfile/EditProfileForm.vue";
+
+export { EditProfileForm };
