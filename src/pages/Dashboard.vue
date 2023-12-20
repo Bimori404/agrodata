290813d6eaec4c5c689c4084c8d4314d7124136f
/Bimori <106877704+Bimori404@users.noreply.github.com>
2023-12-20 17:51:39 -0600

    20122023

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -5,22 +5,23 @@
       <!-- 
         Colores de las graficas
           
-          Temperatura                         -> Gris | Azul
-          Precipitacion                       -> Azul
-          Humedad Realativa                   -> Gris
-          Velocidad y direccion del viento    -> 
-          Radiacion Solar                     -> Naranja/Rojo
+          Temperatura                         -> orange
+          Precipitacion                       -> blue
+          Humedad Realativa                   -> green
+          Velocidad y direccion del viento    -> gray
+          Radiacion Solar                     -> red
+
+          - data-background-color="orange"
+          - data-background-color="blue"
+          - data-background-color="green"
+          - data-background-color="gray"
+          - data-background-color="red"
 
       -->
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
-      >
-        <chart-card
-          :chart-data="dailySalesChart.data"
-          :chart-options="dailySalesChart.options"
-          :chart-type="'Line'"
-          data-background-color="blue"
-        >
+      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33">
+        <chart-card :chart-data="dailyDataLog2.data" :chart-options="dailyDataLog2.options"
+          :chart-type="'Line'" data-background-color="orange">
+
           <template slot="content">
             <h4 class="title">Temperatura</h4>
             <!-- -->
@@ -32,16 +33,9 @@
         </chart-card>
       </div>
       <!----------------------------------------------------------------------->
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
-      >
-        <chart-card
-          :chart-data="emailsSubscriptionChart.data"
-          :chart-options="emailsSubscriptionChart.options"
-          :chart-responsive-options="emailsSubscriptionChart.responsiveOptions"
-          :chart-type="'Bar'"
-          data-background-color="blue"
-        >
+      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33">
+        <chart-card :chart-data="dailyDataLog.data" :chart-options="dailyDataLog.options"
+          :chart-type="'Line'" data-background-color="blue">
           <template slot="content">
             <h4 class="title">Precipitacion</h4>
             <!--  -->
@@ -53,15 +47,9 @@
         </chart-card>
       </div>
       <!----------------------------------------------------------------------->
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
-      >
-        <chart-card
-          :chart-data="dataCompletedTasksChart.data"
-          :chart-options="dataCompletedTasksChart.options"
-          :chart-type="'Line'"
-          data-background-color="green"
-        >
+      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33">
+        <chart-card :chart-data="dailyDataLog.data" :chart-options="dailyDataLog.options"
+          :chart-type="'Line'" data-background-color="green">
           <template slot="content">
             <h4 class="title">Humedad relativa</h4>
             <!-- -->
@@ -73,105 +61,32 @@
         </chart-card>
       </div>
       <!----------------------------------------------------------------------->
-      <!----------------------------------------------------------------------->
-      <!----------------------------------------------------------------------->
-      <div
-        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
-      >
-        <stats-card data-background-color="green">
-          <template slot="header">
-            <!--  -->
-          </template>
-
+      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50">
+        <chart-card :chart-data="dailyDataLog3.data" :chart-options="dailyDataLog3.options"
+          :chart-type="'Line'" data-background-color="gray">
           <template slot="content">
-            <!--  -->
-          </template>
-
-          <template slot="footer">
-            <!-- -->
-          </template>
-        </stats-card>
-      </div>
-      <div
-        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
-      >
-        <stats-card data-background-color="orange">
-          <template slot="header">
-            <!--  -->
-          </template>
-
-          <template slot="content">
-            <!-- -->
-          </template>
-
-          <template slot="footer">
-            <!-- -->
-          </template>
-        </stats-card>
-      </div>
-      <div
-        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
-      >
-        <stats-card data-background-color="red">
-          <template slot="header">
-            <!--  -->
-          </template>
-
-          <template slot="content">
-            <!--  -->
-          </template>
-
-          <template slot="footer">
-            <!-- -->
-          </template>
-        </stats-card>
-      </div>
-      <div
-        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
-      >
-        <stats-card data-background-color="blue">
-          <template slot="header">
-            <!--  -->
-          </template>
-
-          <template slot="content">
-            <!--  -->
-          </template>
-
-          <template slot="footer">
-            <!-- -->
-          </template>
-        </stats-card>
-      </div>
-      <!----------------------------------------------------------------------->
-      <!----------------------------------------------------------------------->
-      <!----------------------------------------------------------------------->
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-      >
-        <md-card>
-          <md-card-header data-background-color="blue">
             <h4 class="title">Velocidad y direccion del viento</h4>
             <!-- -->
-          </md-card-header>
-          <md-card-content>
+          </template>
+
+          <template slot="footer">
             <!-- -->
-          </md-card-content>
-        </md-card>
+          </template>
+        </chart-card>
       </div>
       <!----------------------------------------------------------------------->
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-      >
-        <md-card>
-          <md-card-header data-background-color="orange">
-          <h4 class="title">Radiacion solar</h4>
+      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50">
+        <chart-card :chart-data="dailyDataLog2.data" :chart-options="dailyDataLog2.options"
+          :chart-type="'Line'" data-background-color="red">
+          <template slot="content">
+            <h4 class="title">Radiacion solar</h4>
             <!-- -->
-          </md-card-header>
-          <md-card-content>
+          </template>
+
+          <template slot="footer">
             <!-- -->
-          </md-card-content>
-        </md-card>
+          </template>
+        </chart-card>
       </div>
     </div>
   </div>
@@ -179,7 +94,7 @@
 
 <script>
 import {
-  StatsCard,
+  // StatsCard,
   ChartCard,
   // NavTabsCard,
   // NavTabsTable,
@@ -188,7 +103,7 @@ import {
 
 export default {
   components: {
-    StatsCard,
+    // StatsCard,
     ChartCard,
     // NavTabsCard,
     // NavTabsTable,
@@ -198,10 +113,10 @@ export default {
   // Info que se muestra en el contenido //
   data() {
     return {
-      dailySalesChart: {
+      dailyDataLog: {
         data: {
-          labels: ["M", "T", "W", "T", "F", "S", "S"],
-          series: [[12, 17, 7, 17, 23, 18, 38]],
+          labels: ["00", "03", "06", "09", "12", "15", "18", "21"],
+          series: [[12, 17, 7, 17, 23, 18, 38, 22]],
         },
         options: {
           lineSmooth: this.$Chartist.Interpolation.cardinal({
@@ -217,10 +132,10 @@ export default {
           },
         },
       },
-      dataCompletedTasksChart: {
+      dailyDataLog2: {
         data: {
-          labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
-          series: [[230, 750, 450, 300, 280, 240, 200, 190]],
+          labels: ["00", "03", "06", "09", "12", "15", "18", "21"],
+          series: [[12, 17, 7, 17, 23, 18, 38, 22]],
         },
 
         options: {
@@ -228,7 +143,7 @@ export default {
             tension: 0,
           }),
           low: 0,
-          high: 1000,
+          high: 50,
           chartPadding: {
             top: 0,
             right: 0,
@@ -237,52 +152,25 @@ export default {
           },
         },
       },
-      emailsSubscriptionChart: {
+      dailyDataLog3: {
         data: {
-          labels: [
-            "Ja",
-            "Fe",
-            "Ma",
-            "Ap",
-            "Mai",
-            "Ju",
-            "Jul",
-            "Au",
-            "Se",
-            "Oc",
-            "No",
-            "De",
-          ],
-          series: [
-            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-          ],
+          labels: ["00", "03", "06", "09", "12", "15", "18", "21"],
+          series: [[12, 17, 7, 17, 23, 18, 38, 42]],
         },
+
         options: {
-          axisX: {
-            showGrid: false,
-          },
+          lineSmooth: this.$Chartist.Interpolation.cardinal({
+            tension: 0,
+          }),
           low: 0,
-          high: 1000,
+          high: 50,
           chartPadding: {
             top: 0,
-            right: 5,
+            right: 0,
             bottom: 0,
             left: 0,
           },
         },
-        responsiveOptions: [
-          [
-            "screen and (max-width: 640px)",
-            {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc: function (value) {
-                  return value[0];
-                },
-              },
-            },
-          ],
-        ],
       },
     };
   },
